eebacb3a5ad78b49b721285730b55b272c533db9
Anthony Nixon <ajnixon@spscommerce.com>
2019-03-08 11:40:22 -0600

    Add signup method

--- main.go
+++ main.go
@@ -25,101 +25,116 @@ type Message struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
+var DB_USER = os.Getenv("PYRAMID_CHAT_DB_USER")
+var DB_PASS = os.Getenv("PYRAMID_CHAT_DB_PASS")
+var DB_HOST = os.Getenv("PYRAMID_CHAT_DB_HOST")
+var DB_NAME = os.Getenv("PYRAMID_CHAT_DB_NAME")
+
+var dsn = DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":3306)/" + DB_NAME + "?parseTime=true"
+
+
 func main() {
-	DB_USER := os.Getenv("PYRAMID_CHAT_DB_USER")
-	DB_PASS := os.Getenv("PYRAMID_CHAT_DB_PASS")
-	DB_HOST := os.Getenv("PYRAMID_CHAT_DB_HOST")
-	DB_NAME := os.Getenv("PYRAMID_CHAT_DB_NAME")
-
-	dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":3306)/" + DB_NAME + "?parseTime=true"
-
-
 	r := gin.Default()
 	r.GET("/messages", func(c *gin.Context) {
-		var (
-			message Message
-			messages []Message
-		)
-		// Open DB connection
-		db, err := sql.Open("mysql", dsn)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		defer db.Close()
-
-		// make sure our connection is available
-		err = db.Ping()
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-
-		rows, err := db.Query("select id, user_id, user_name, content, timestamp FROM messages")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			err = rows.Scan(&message.Id, &message.User_id, &message.Username, &message.Content, &message.Timestamp)
-			messages = append(messages, message)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"messages": messages,
-		})
+		getMessages(c)
 	})
 
 	r.POST("/messages", func(c *gin.Context) {
-		var message NewMessage
-		c.BindJSON(&message)
+		postMessage(c)
+	})
 
-		// Open DB connection
-		db, err := sql.Open("mysql", dsn)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		defer db.Close()
-
-		// make sure our connection is available
-		err = db.Ping()
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-
-		// Get the username from the DB
-		var username string
-		var num_chars int
-		var password string
-		rows, err := db.Query("select username, num_chars, password FROM users where id = ?", message.User_id)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-		rows.Next()
-		err = rows.Scan(&username, &num_chars, &password)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if message.Password != password {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
-
-		if len(message.Content) > num_chars {
-			c.JSON(http.StatusBadRequest, gin.H{"status": "too_many_chars"})
-			return
-		}
-
-		stmt, err := db.Prepare("insert into messages (user_id, user_name, content) values(?,?,?);")
-		_, err = stmt.Exec(message.User_id, username, message.Content)
-
-
-		c.JSON(http.StatusCreated, gin.H{"status": "created"})
+	r.POST("/signup", func(c *gin.Context) {
+		signUp(c)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
+func getMessages (c *gin.Context) {
+	var (
+		message Message
+		messages []Message
+	)
+	// Open DB connection
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+
+	// make sure our connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	rows, err := db.Query("select id, user_id, user_name, content, timestamp FROM messages")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&message.Id, &message.User_id, &message.Username, &message.Content, &message.Timestamp)
+		messages = append(messages, message)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"messages": messages,
+	})
+}
+
+func postMessage (c *gin.Context) {
+	var message NewMessage
+	c.BindJSON(&message)
+
+	// Open DB connection
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+
+	// make sure our connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	// Get the username from the DB
+	var username string
+	var num_chars int
+	var password string
+	rows, err := db.Query("select username, num_chars, password FROM users where id = ?", message.User_id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	rows.Next()
+	err = rows.Scan(&username, &num_chars, &password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if message.Password != password {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		return
+	}
+
+	if len(message.Content) > num_chars {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "too_many_chars"})
+		return
+	}
+
+	stmt, err := db.Prepare("insert into messages (user_id, user_name, content) values(?,?,?);")
+	_, err = stmt.Exec(message.User_id, username, message.Content)
+
+
+	c.JSON(http.StatusCreated, gin.H{"status": "created"})
+}
+
+func signUp(c *gin.Context) {
+
+}
